a803a5a4e56972a4f5ceea8a06a7f8353133cba4
Baiburtian Violetta <bayviol@yandex.ru>
2023-09-25 13:14:40 +0300

    fix: delete comments

--- src/widgets/postAbout/Collapse.tsx
+++ src/widgets/postAbout/Collapse.tsx
@@ -1,13 +1,9 @@
 import type {CollapseProps} from 'antd';
-import {Button, Card, Collapse, List, Spin} from 'antd';
-import {PostsList} from "../../shared/types/postsList";
-import React, {useEffect, useState} from "react";
+import {Collapse} from 'antd';
+import React from "react";
 import {Post} from "../../shared/types/post";
-import InfiniteScroll from "react-infinite-scroll-component";
-import {Link} from "react-router-dom";
 
-
-function Collapses(post:Post) {
+function Collapses(post: Post) {
     const title = (
         <p style={{paddingLeft: 24}}>
             {post.title}
@@ -43,12 +39,11 @@ function Collapses(post:Post) {
         },
     ];
 
-
     return (
-        <Collapse style={{backgroundColor:"white", width: "70%",margin:"2%"}} items={items} bordered={false} defaultActiveKey={['1']}/>
+        <Collapse style={{backgroundColor: "white", width: "70%", margin: "2%"}} items={items} bordered={false}
+                  defaultActiveKey={['1']}/>
 
     );
 }
-
 
 export default Collapses;
